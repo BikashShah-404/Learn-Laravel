e167514c44e1f2b216f4cec63e912790239869e6
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-27 12:43:39 +0545

    Using Blade directives to conditionally render tag

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active'=>false,'type'=>'button'])
 
-<?php if($type==="link"): ?>
+@if($type="link")
     <a class="{{$active ? 'bg-blue-800 p-2 rounded-2xl text-white' :'p-2'}}" {{$attributes}}>{{$slot}}</a> 
-<?php else : ?>
+@else
     <button class="{{$active ? 'bg-blue-800 p-2 rounded-2xl text-white' :'p-2'}}" {{$attributes}}>{{$slot}}</button>
-<?php endif ?>
+@endif
