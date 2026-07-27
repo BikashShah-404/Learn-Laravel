104ab1af56718b7d2fd28f4035b25930404cbed4
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-27 12:41:44 +0545

    Using Traditional php syntax to render tags conditionally

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,9 +8,9 @@
 </head>
 <body>
     <nav class="w-full bg-blue-400 p-4 flex gap-4 text-lg">
-        <x-nav-link href="/home" :active="request()->is('home')">Home</x-nav-link>
-        <x-nav-link href="/about" :active="request()->is('about')" >About</x-nav-link>
-        <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link>
+        <x-nav-link href="/home" :active="request()->is('home')" type="link">Home</x-nav-link>
+        <x-nav-link href="/about" :active="request()->is('about')" type="link" >About</x-nav-link>
+        <x-nav-link :active="request()->is('contact')" type="button" >Contact</x-nav-link>
     </nav>
     <div class="bg-green-200 h-40 flex items-center justify-center">
         <h1 class="text-4xl ">{{$heading}}</h1>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,7 @@
-@props(['active'=>false])
+@props(['active'=>false,'type'=>'button'])
 
-<a class="{{$active ? 'bg-blue-800 p-2 rounded-2xl text-white' :'p-2'}}" {{$attributes}}>{{$slot}}</a> 
+<?php if($type==="link"): ?>
+    <a class="{{$active ? 'bg-blue-800 p-2 rounded-2xl text-white' :'p-2'}}" {{$attributes}}>{{$slot}}</a> 
+<?php else : ?>
+    <button class="{{$active ? 'bg-blue-800 p-2 rounded-2xl text-white' :'p-2'}}" {{$attributes}}>{{$slot}}</button>
+<?php endif ?>
