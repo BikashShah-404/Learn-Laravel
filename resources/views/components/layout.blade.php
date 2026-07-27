aa960c04d8e18661cffada45a6e5b4657b64ad77
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-27 11:46:20 +0545

    Passing attributes from child compt to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <nav>
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <x-nav-link href="/home">Home</x-nav-link>
+        <x-nav-link href="/about" style="color: red;">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
     </nav>
     {{$slot}}
 </body>
