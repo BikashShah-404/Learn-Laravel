06c1ce94c535f92b48aa52b417590411c6c39519
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-27 16:44:30 +0545

    DRY by declaring var at top

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,24 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$jobs=[['id' => '1', 'title' => 'Software Engineer', 'salary' => 'NPR 1,20,000'], ['id' => '2', 'title' => 'AI Engineer', 'salary' => 'NPR 1,50,000'], ['id' => '3', 'title' => 'SEO Engineer', 'salary' => 'NPR 1,00,000']];
+
+
 Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => [['id' => '1', 'title' => 'Software Engineer', 'salary' => 'NPR 1,20,000'], ['id' => '2', 'title' => 'AI Engineer', 'salary' => 'NPR 1,50,000'], ['id' => '3', 'title' => 'SEO Engineer', 'salary' => 'NPR 1,00,000']]]);
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', ['jobs' =>$jobs]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs=[['id' => '1', 'title' => 'Software Engineer', 'salary' => 'NPR 1,20,000'], ['id' => '2', 'title' => 'AI Engineer', 'salary' => 'NPR 1,50,000'], ['id' => '3', 'title' => 'SEO Engineer', 'salary' => 'NPR 1,00,000']];
-
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job=Arr::first($jobs, fn($job) => $job['id'] == $id);
     return view('job',['job'=>$job]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about'); 
 });
 
 Route::get('/contact', function () {
