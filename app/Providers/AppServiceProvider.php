535901191d2beee0aed48892506c1b6f3b5d3c81
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-29 13:16:19 +0545

    Eager Loading and N+1 Problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
+
+// We can think of this file as a file for configuring our application:
+// Disabling the lazy loading.
+// We can do this in boot method, the boot method is triggered after all of the project dependencies have been fully loaded
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // On doing this, when we try to lazy load, we get error and hence we need to eager load our data then
+        Model::preventLazyLoading();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,13 @@ Route::get('/home', function () {
 });
 
 Route::get('/jobs', function ()  {
-    return view('jobs', ['jobs' =>Job::all()]);
+    // We will implement eager loadin, that is load all jobs along with the employers data in the same query
+
+    $jobs=Job::with('employer')->get(); //get() is like select *, so if million data is their, all will load , we need to apply some limiting and pagination and all
+    
+    // Well on doing this , we got all job along with employer details from the single query , this is eager loading. If we want we can disable lazy loading entirely from our project if we think that doesn't serve any puropse in our app context, or we could use lazy loading carefully since it does provides benefit too.
+
+    return view('jobs', ['jobs' =>$jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
