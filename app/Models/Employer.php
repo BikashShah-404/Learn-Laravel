a0420c64ee4f426a90fff9436375066cee4133e5
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-29 10:44:53 +0545

    Two Key Eloquent Relationship

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,4 +9,18 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    /*
+     The relations that we are defining here in the modesl, now in the php artisna tinker, when we write 
+     $employer=App\Models\Employer::first(); 
+     $employer->jobs; this essentially executs a SQL query which tells it to execute SELECT * FROM jobs WHERE employer_id = ? with the employer's ID bound as a parameter (not string interpolation). This is safer against SQL injection..The point is basically its executing the sql query behind the scence, Also there comes a concept of lazy loading so essentially ,the query is parsed but it gets executed only when we request/access the property $employer->jobs.    
+    */
+    public function jobs(){
+        return $this->hasMany(Job::class);
+    }
+
+    /* 
+    We will get a collection of Jobs in return which we can interact as an array or even as an collection.
+    so $employer->jobs[0]->title 
+       $employer->jobs->first() , first() is one of the methods defined on the collection  */
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,9 @@ class Job extends Model{
   protected $table='job_listings';
 
   protected $fillable=['title','salary'];
+
+  // We say Job Belongs to Employer, so belongsTo, also in reverse we can say an Employer has many Jobs, so in employer Model ,we could use $this->hasMany(Job::class), similary we have many such relations definitions Eloquent has provide us with.
+  public function employer(){
+    return $this->belongsTo(Employer::class);
+  }
 }
