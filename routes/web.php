6a47769c094c027ea5b0d2e7fccbf4f69249597e
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-30 17:24:50 +0545

    Route Group

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::view("/home",'home');
 
-Route::get('/jobs', [JobController::class,"index"]);
-Route::get('/jobs/create',[JobController::class,"create"]);
-Route::get('/jobs/{job:id}',[JobController::class,"show"] );
-Route::post('/jobs', [JobController::class,"store"]);
-Route::get('/jobs/{job}/edit', [JobController::class,"edit"]);
-Route::patch('/jobs/{job}',[JobController::class,"update"] );
-Route::delete('/jobs/{job}', [JobController::class,"destroy"] );
+// Here we have been repeating our controller names in our routes right, better would be to use "Route Group" as :
+Route::controller(JobController::class)->group(function(){
+    Route::get('/jobs', "index");
+    Route::get('/jobs/create',"create");
+    Route::get('/jobs/{job:id}',"show" );
+    Route::post('/jobs', "store");
+    Route::get('/jobs/{job}/edit',"edit");
+    Route::patch('/jobs/{job}',"update" );
+    Route::delete('/jobs/{job}',"destroy" );
+});
+
 
 Route::view("/about",'about');
 Route::view("/contact",'contact');
 
-
-// Listing our routes :
-// php artisan route:list - list our app routes but also routes related to third party packages.
-// To get our app routes only:
-// php artisan route:list --except-vendor 
