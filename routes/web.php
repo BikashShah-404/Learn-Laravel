a957641c71c8469f454846a3b25813152e2a3e01
BikashShah-404 <shahbikash4659@gmail.com>
2026-07-30 17:18:04 +0545

    ROute::view()

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
-    return view('home'); 
-});
+// Often we get into the situations where we listen for a get request and do nothing expect returning a view , for such situations we can use Route::view
+// Route::get('/home', function () {
+//     return view('home'); 
+// });
+
+Route::view("/home",'home');
 
 Route::get('/jobs', [JobController::class,"index"]);
 Route::get('/jobs/create',[JobController::class,"create"]);
@@ -15,10 +18,10 @@ Route::get('/jobs/{job}/edit', [JobController::class,"edit"]);
 Route::patch('/jobs/{job}',[JobController::class,"update"] );
 Route::delete('/jobs/{job}', [JobController::class,"destroy"] );
 
-Route::get('/about', function () {
-    return view('about'); 
-});
+Route::view("/about",'about');
+Route::view("/contact",'contact');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+
+
+
+
